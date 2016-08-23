ad577c87b1331cf2da0fb38b55433c7087be120f
cloud.zhang <1032863320@qq.com>
2016-08-23 16:49:42 +0800

    init this project

---
+++ launcher3/src/main/java/com/vdroid/launcher3/OnAlarmListener.java
@@ -0,0 +1,5 @@
+package com.vdroid.launcher3;
+
+public interface OnAlarmListener {
+    public void onAlarm(Alarm alarm);
+}
---
+++ launcher3/src/main/java/com/vdroid/launcher3/ToggleWeightWatcher.java
@@ -0,0 +1,7 @@
+package com.vdroid.launcher3;
+
+import android.app.Activity;
+
+public class ToggleWeightWatcher extends Activity {
+
+}
